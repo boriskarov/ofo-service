0d5f4f920d8a5aefb6277f7478e88c61f6ce6394
isidoradjokic <isidora.djokic@gmail.com>
2024-01-17 17:18:55 +0100

    OFO-service-setup

---
+++ src/main/java/com/example/ofo/OfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ofo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ofo/OfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ofo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
